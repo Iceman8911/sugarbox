a586a1553d27257a9c7b98f3fded638da9ca0bcb
Iceman8911 <wuchiJSS3@gmail.com>
2025-08-04 23:22:32 +0100

    test(if-engine): working on some tests

--- test/engine/if-engine.test.ts
+++ test/engine/if-engine.test.ts
@@ -13,26 +13,60 @@ async function initEngine() {
 			},
 		],
 		startPassage: { name: "Start", passage: "This is the start passage" },
-		variables: { name: "Dave", inventory: { gold: 123, gems: 12 } },
+		variables: {
+			name: "Dave",
+			age: 21,
+			class: "Paladin",
+			level: 6,
+			location: "Tavern",
+			inventory: {
+				gold: 123,
+				gems: 12,
+				items: ["Black Sword", "Slug Shield", "Old Cloth"],
+			},
+		},
+		config: {
+			maxStateCount: 1_000_000,
+		},
 	});
 }
 
 describe("SugarboxEngine", () => {
-	test("should initialize without issue", async () => {
+	test("story variables should be set without issue and persist after passage navigation", async () => {
 		const engine = await initEngine();
 
 		engine.setVars((state) => {
-			state.name = "Sheep";
+			state.name = "Bob";
 
-			state.inventory.gems = 21;
+			state.inventory.gems++;
+
+			state.inventory.items.push("Overpowered Sword");
 		});
+
+		engine.navigateTo("Another Passage");
+
+		expect(
+			engine.vars.inventory.items.includes("Overpowered Sword"),
+		).toBeTrue();
+
+		expect(engine.vars.inventory.gems).toBe(13);
+
+		expect(engine.vars.name).toBe("Bob");
+	});
+
+	test("passage navigation should increment the index", async () => {
+		const engine = await initEngine();
 
 		engine.navigateTo("Another Passage");
 
 		expect(engine.passageId).toBe("Another Passage");
 
-		engine.navigateTo("Start");
+		for (let i = 1; i < 1_000; i++) {
+			engine.navigateTo(
+				["Another Passage", "Forest Path", "Mountain Peak"][i % (3 + 1)],
+			);
+		}
 
-		expect(engine.index).toBe(2);
+		expect(engine.index).toBe(1_000);
 	});
 });
