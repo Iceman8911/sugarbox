d557766b5601517c56eb2c3a6e4e147b0e03ee4b
Iceman8911 <wuchiJSS3@gmail.com>
2025-08-05 10:02:52 +0100

    test(mocks): add cache adapter mock

---
+++ test/mocks/cache.ts
@@ -0,0 +1,29 @@
+import { mock } from "bun:test";
+import QuickLru from "quick-lru";
+import type { CacheAdapter } from "../../src/types/adapters";
+
+const cacheAdapter: () => CacheAdapter<string, string> = mock(() => {
+	const store = new QuickLru<string, string>({ maxSize: 10 });
+
+	const adapter: CacheAdapter<string, string> = {
+		get(key) {
+			return store.get(key);
+		},
+
+		set(key, val) {
+			store.set(key, val);
+		},
+
+		delete(key) {
+			store.delete(key);
+		},
+
+		clear() {
+			store.clear();
+		},
+	};
+
+	return adapter;
+});
+
+export { cacheAdapter };
