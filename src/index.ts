9f4b864353d871945ba911f008b34608eca292e7
Iceman8911 <wuchiJSS3@gmail.com>
2025-08-10 16:57:23 +0100

    fix: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { SugarboxEngine } from "./engine/if-engine";
+import type {
+	SugarBoxCacheAdapter,
+	SugarBoxPersistenceAdapter,
+} from "./types/adapters";
 import type { SugarBoxConfig } from "./types/if-engine";
+import {
+	SugarBoxCompatibleClassConstructorCheck,
+	SugarBoxCompatibleClassInstance,
+} from "./types/userland-classes";
 
-export { SugarboxEngine, type SugarBoxConfig };
+export {
+	SugarboxEngine,
+	type SugarBoxConfig,
+	SugarBoxCompatibleClassConstructorCheck,
+	SugarBoxCompatibleClassInstance,
+	type SugarBoxCacheAdapter,
+	type SugarBoxPersistenceAdapter,
+};
